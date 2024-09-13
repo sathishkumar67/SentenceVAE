79b9cc3b29fbbc94847bc96a4792facc22132836
sathishkumar67 <sathishvillers2000@gmail.com>
2024-09-13 21:27:42 +0530

    removed dtype and device

--- positional_encodings.py
+++ positional_encodings.py
@@ -7,16 +7,11 @@ class PositionalEncoding(nn.Module):
     def __init__(
         self, 
         hidden_size: int, 
-        max_len: int = 4096,
-        device: torch.device = None,
-        dtype: torch.dtype = None
+        max_len: int = 4096
     ):
         super(PositionalEncoding, self).__init__()
         assert hidden_size % 2 == 0, \
             f"Cannot use sin/cos positional encoding with odd hidden_size (go size={hidden_size})."
-        
-        device = device if device is not None else torch.device('cuda')
-        dtype = dtype if dtype is not None else torch.float16
 
         pe = torch.zeros(max_len, hidden_size)
         position = torch.arange(0, max_len).unsqueeze(1)
@@ -27,10 +22,6 @@ class PositionalEncoding(nn.Module):
         
         self.pe = pe.unsqueeze(0)
         self.pe.requires_grad = False
-
-        self.pe = self.pe.to(dtype)
-        self.pe = self.pe.to(device)
-    
 
     def forward(self, seq_len):
         return self.pe[:, :seq_len, :]
